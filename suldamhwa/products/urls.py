1596c5d882e4e128d4ea0ac53aa29a814d936160
MinseokChoi <alex0316@gmail.com>
2022-05-07 18:11:53 +0900

    Add: README.md

--- suldamhwa/products/urls.py
+++ suldamhwa/products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from products.views import ProductListView
+from products.views import ProductListView, ProductDetailView
 
 urlpatterns = [
-    path('/list', ProductListView.as_view())
+    path('/list', ProductListView.as_view()),
+    path('/<int:product_id>', ProductDetailView.as_view()),
     ]
